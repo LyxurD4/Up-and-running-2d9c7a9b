c85e45c4443a72bb71cacf6bd90f57afeb649e20
LyxurD4 <mdoes@roc-dev.com>
2020-02-28 08:55:01 +0100

    verbetering

--- index.php
+++ index.php
@@ -7,22 +7,22 @@
 </head>
 <body>
 <?php
-    require_once 'dbconfig.php';
-    
-    $dsn= "mysql:host=$host;dbname=$db";
-    
-    try {
-    // create a PDO connection with the configuration data
-        $conn = new PDO($dsn, $username, $password);
-    
-    // display a message if connected to database successfully
-    if ($conn) {
-        echo "Connected to the <strong>$db</strong> database successfully!";
-    }
-    } catch (PDOException $e) {
-        // report error message
-        echo $e->getMessage();
-    }
+require_once 'dbconfig.php';
+
+$dsn= "mysql:host=$host;dbname=$db";
+
+try {
+// create a PDO connection with the configuration data
+    $conn = new PDO($dsn, $username, $password);
+
+// display a message if connected to database successfully
+if ($conn) {
+    echo "Connected to the <strong>$db</strong> database successfully!";
+}
+} catch (PDOException $e) {
+    // report error message
+    echo $e->getMessage();
+}
 ?>
 </body>
 </html>
